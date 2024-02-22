2af1283deb2a1615d79bd6aa708dec39ee867a08
jamesdgardner <jdg19@geneseo.edu>
2024-02-22 15:19:26 -0500

    nice

---
+++ RforDSBookPractice.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+palmerpenguins::penguins
+view(penguins)
+?penguins
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+  ) +
+  geom_point(mapping = aes(color = species, shape = species)) +
+  geom_smooth(method = "lm") +
+  labs( 
+  title = "Body Mass & Flipper Length",
+  subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+  x = "Flipper Length (mm)", y = "Body Mass (g)",
+  color = "Species", shape = "Species"
+) +
+  scale_color_colorblind()
+
